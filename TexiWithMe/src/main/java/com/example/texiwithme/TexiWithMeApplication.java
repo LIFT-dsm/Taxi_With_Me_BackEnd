9ca192d62185a5a4b2505eecd1fde6306bedf2ca
오송주 <dhthdwn7920@gmail.com>
2024-04-01 18:36:34 +0900

    💡 :: project setting

---
+++ TexiWithMe/src/main/java/com/example/texiwithme/TexiWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.texiwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexiWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TexiWithMeApplication.class, args);
+    }
+
+}
---
+++ TexiWithMe/src/test/java/com/example/texiwithme/TexiWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.texiwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexiWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
