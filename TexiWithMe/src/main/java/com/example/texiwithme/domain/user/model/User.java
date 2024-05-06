8cc26410d2505e31625b094376bd38eeeed4c6cc
김승원 <107746917+ori0o0p@users.noreply.github.com>
2024-05-06 22:23:57 +0900

    Update User.java

--- TexiWithMe/src/main/java/com/example/texiwithme/domain/user/model/User.java
+++ TexiWithMe/src/main/java/com/example/texiwithme/domain/user/model/User.java
@@ -3,41 +3,41 @@ package com.example.texiwithme.domain.user.model;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-@Data
+@Getter
 @Entity
 @DynamicInsert
 @DynamicUpdate
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 public class User {
     @Id
     @Column(name = "user_id")
-    @GeneratedValue
-    long userId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
 
     @Column(nullable = false, name = "stu_id")
-    int stdId;
+    private int stdId;
 
     @Column(nullable = false, length = 20)
-    String name;
+    private String name;
 
     @Column(nullable = false, length = 20)
-    String username;
+    private String username;
 
     @Column(nullable = false)
-    String password;
+    private String password;
 
     @Enumerated(value = EnumType.STRING)
     @Column(nullable = false)
-    Gender gender;
+    private Gender gender;
 
     @Column(nullable = false)
-    String profile;
+    private String profile;
+
+    protected User() {}
 
     @Builder
     public User (int stdId, String name, String username, String password, Gender gender, String profile) {
@@ -46,6 +46,6 @@ public class User {
         this.username = username;
         this.password = password;
         this.gender = gender;
-        this.profile = profile == null ? "기본 이미지 구현 예정" : profile;
+        this.profile = profile.isBlank() ? "기본 이미지 구현 예정" : profile;
     }
 }
