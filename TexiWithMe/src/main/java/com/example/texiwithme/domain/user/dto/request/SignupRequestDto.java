c55969241ebca5043e36670dc89ae8cfffa04ec0
김승원 <107746917+ori0o0p@users.noreply.github.com>
2024-05-07 17:11:02 +0900

    Update SignupRequestDto.java

--- TexiWithMe/src/main/java/com/example/texiwithme/domain/user/dto/request/SignupRequestDto.java
+++ TexiWithMe/src/main/java/com/example/texiwithme/domain/user/dto/request/SignupRequestDto.java
@@ -1,13 +1,10 @@
 package com.example.texiwithme.domain.user.dto.request;
 
-import com.example.texiwithme.domain.user.model.Gender;
-import lombok.Data;
-
-@Data
-public class SignupRequestDto {
-    private int studentId;
-    private String name;
-    private String nickname;
-    private String password;
-    private String gender;
+public record SignupRequestDto(
+    int studentId,
+    String name,
+    String nickname,
+    String password,
+    String gender
+) {
 }
