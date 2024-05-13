41d71b7fd8481ae1bc37f564c6762388a51cbaae
101 <tmddnjs001133@dsm.hs.kr>
2024-05-13 18:17:13 +0900

    refactor :: 사용하지 않는 라인 삭제

--- TexiWithMe/src/main/java/com/example/texiwithme/global/security/jwt/JwtProvider.java
+++ TexiWithMe/src/main/java/com/example/texiwithme/global/security/jwt/JwtProvider.java
@@ -12,7 +12,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 
 import java.security.Key;
